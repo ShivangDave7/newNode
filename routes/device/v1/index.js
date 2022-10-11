f00e3ad08c37ce7699c00e606877037d7e589c0c
ShivangDave7 <tejaswi.tandel111@gmail.com>
2022-10-11 06:29:51 +0000

    Code Synced by DhiWise

---
+++ routes/device/v1/index.js
@@ -0,0 +1,30 @@
+/**
+ * index.js
+ * @description :: index route file of device platform.
+ */
+
+const express =  require('express');
+const router =  express.Router();
+router.use('/device/auth',require('./auth'));
+router.use(require('./encounterRoutes'));
+router.use(require('./departmentsRoutes'));
+router.use(require('./medicationRoutes'));
+router.use(require('./enterpriseRoutes'));
+router.use(require('./orderRoutes'));
+router.use(require('./noteRoutes'));
+router.use(require('./patientRoutes'));
+router.use(require('./orderItemRoutes'));
+router.use(require('./CustomerRoutes'));
+router.use(require('./TaskRoutes'));
+router.use(require('./CommentRoutes'));
+router.use(require('./PlanRoutes'));
+router.use(require('./Chat_groupRoutes'));
+router.use(require('./Chat_messageRoutes'));
+router.use(require('./ToDoRoutes'));
+router.use(require('./Appointment_scheduleRoutes'));
+router.use(require('./Appointment_slotRoutes'));
+router.use(require('./EventRoutes'));
+router.use(require('./MasterRoutes'));
+router.use(require('./BlogRoutes'));
+
+module.exports = router;
